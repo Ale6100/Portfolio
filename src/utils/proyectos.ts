a01552f393f1b29896bf3137d24b465bae7c1d32
Alejandro <alejandro_portaluppi@outlook.com>
2023-10-30 12:30:51 -0300

    Proyecto 'Lista de Notas' agregado

--- src/utils/proyectos.ts
+++ src/utils/proyectos.ts
@@ -4,14 +4,14 @@ const proyectos = [
         img: "./img/proyecto_full.webp",
         linkGitHub: "https://github.com/Ale6100/Curso-backend.git",
         linkSitio: "https://proyectocompleto.netlify.app/",
-        herramientas: ["CSS", "JavaScript", "TypeScript", "NodeJS", "MongoDB", "ExpressJS", "Bycrypt", "Cors", "Dotenv", "EJS", "Jsonwebtoken", "Minimist", "Multer", "Nodemailer", "Stripe", "Swagger", "Winston", "ReactJS", "Tailwind", "Vite", "Moment", "Toastify"]
+        herramientas: ["HTML", "CSS", "JavaScript", "TypeScript", "NodeJS", "MongoDB", "ExpressJS", "Bycrypt", "Cors", "cookie-parser", "Dotenv", "EJS", "Jsonwebtoken", "Minimist", "Multer", "Nodemailer", "Stripe", "Swagger", "Winston", "ReactJS", "Tailwind", "Vite", "Moment", "Toastify", "copyfiles"]
     },
     {
         titulo: 'Juego "Buscagatos"',
         img: "./img/Buscagatos.webp",
         linkGitHub: "https://github.com/Ale6100/Buscagatos_primer_juego_JS.git",
         linkSitio: "https://buscagatos.netlify.app/",
-        herramientas: ["HTML", "CSS", "JavaScript", "Sweet Alert 2", "Toastify"]
+        herramientas: ["HTML", "CSS", "JavaScript", "Sweet Alert 2", "Toastify", "@ts-check"]
     },
     {
         titulo: 'Módulo en npm',
@@ -25,21 +25,28 @@ const proyectos = [
         img: "./img/simuladorPlazoFijo.webp",
         linkGitHub: "https://github.com/Ale6100/Simulador-plazo-fijo.git",
         linkSitio: "https://simuladorplazofijo.netlify.app/",
-        herramientas: ["HTML", "CSS", "JavaScript", "Chart.js", "MathJax", "Polyfill.io", "Sweet Alert 2", "Toastify"]
+        herramientas: ["HTML", "CSS", "JavaScript", "TypeScript", "ReactJS", "NodeJs", "Tailwind", "Vite", "echarts", "react-katex", "react-toastify"]
     },
     {
         titulo: 'Reproductor de videos',
         img: "./img/reproductorDeVideos.webp",
         linkGitHub: "https://github.com/Ale6100/Reproductor-de-videos.git",
         linkSitio: "https://reproductordevideos.netlify.app/",
-        herramientas: ["CSS", "TypeScript", "ReactJS", "NodeJS", "Tailwind", "Vite", "Framer Motion", "Lodash"]
+        herramientas: ["HTML", "CSS", "JavaScript", "TypeScript", "ReactJS", "NodeJS", "Tailwind", "Vite", "Framer Motion", "Lodash"]
     },
     {
         titulo: 'Sala de chat',
         img: "./img/salaDeChat.webp",
         linkGitHub: "https://github.com/Ale6100/Chat-js.git",
         linkSitio: "https://chat-ts.netlify.app/",
-        herramientas: ["CSS", "TypeScript", "JavaScript", "MongoDB", "Socket.io", "Express", "NodeJS", "EJS", "dotenv", "winston", "cors", "ReactJS", "Vite", "Tailwind", "Toastify", "Sweet Alert 2"]
+        herramientas: ["HTML", "CSS", "JavaScript", "TypeScript", "MongoDB", "Socket.io", "ExpressJS", "NodeJS", "EJS", "dotenv", "winston", "cors", "ReactJS", "Vite", "Tailwind", "Toastify", "Sweet Alert 2", "copyfiles"]
+    },
+    {
+        titulo: "Lista de Notas",
+        img: "./img/listaDeNotas.webp",
+        linkGitHub: "https://github.com/Ale6100/Lista-de-notas.git",
+        linkSitio: "https://lista-de-notas.netlify.app/",
+        herramientas: ["HTML", "CSS", "JavaScript", "TypeScript", "ReactJS", "NodeJs", "Tailwind", "Vite", "react-router-dom", "react-toastify", "sweetalert2", "mongoose", "ExpressJS", "cookie-parser", "cors", "dotenv", "jsonwebtoken", "bcrypt", "winston"]
     },
     {
         titulo: 'Asistente virtual',
