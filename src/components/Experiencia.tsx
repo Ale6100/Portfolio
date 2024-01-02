5c15be96279526a933f75276f117d10649fad776
Alejandro Portaluppi <alejandro_portaluppi@outlook.com>
2024-01-02 18:14:21 -0300

    Información laboral actualizada

--- src/components/Experiencia.tsx
+++ src/components/Experiencia.tsx
@@ -8,7 +8,7 @@ const Experiencia = () => {
             <div className="flex flex-col w-full px-10 gap-10 max-md:px-5 max-md:gap-5">
             {experiencia.map((exp, i) => (
                 <div className="flex items-center" key={i}>
-                    <img loading='lazy' className='w-32 max-md:w-16' src={exp.img ? `/img/${exp.img}` : "https://cdn-icons-png.flaticon.com/512/189/189792.png"} alt={`Imagen ${exp.nombre}`} />
+                    <img loading='lazy' className='w-32 max-md:w-16 rounded-sm' src={exp.img ? `/img/${exp.img}` : "https://cdn-icons-png.flaticon.com/512/189/189792.png"} alt={`Imagen ${exp.nombre}`} />
                     <div className="ml-10 max-md:ml-5">
                         <h4 className='font-medium'>{exp.nombre}</h4>
                         <p>{exp.puesto}</p>
--- src/utils/experiencia.ts
+++ src/utils/experiencia.ts
@@ -1,18 +1,32 @@
-const experiencia = [
+interface IExperiencia {
+    img: string
+    nombre: string
+    puesto: string
+    fechaInicio: string
+    fechaFin?: string
+    freelance?: boolean
+}
+
+const experiencia: IExperiencia[] = [
+    {
+        img: "minDef.webp",
+        nombre: "Ministerio de Defensa",
+        puesto: "Desarrollador Full Stack",
+        fechaInicio: "12/2023",
+    },
     {
         img: "unahur.webp",
         nombre: "Universidad Nacional de Hurlingham",
         puesto: "Desarrollador Junior",
-        fechaInicio: "01/07/2023",
-        fechaFin: "01/10/2023",
-        freelance: false
+        fechaInicio: "07/2023",
+        fechaFin: "10/2023",
     },
     {
         img: "DevsAnts.webp",
         nombre: "Devs Ants",
         puesto: "Backend Developer",
-        fechaInicio: "01/11/2022",
-        fechaFin: "01/07/2023",
+        fechaInicio: "11/2022",
+        fechaFin: "07/2023",
         freelance: true
     }
 ]
